06035dd75d0772a7a6ddbd5f54c304fac0f1797f
David Tung <isdaviddong@gmail.com>
2025-04-14 19:32:35 +0800

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -27,6 +27,7 @@ public class IndexModel : PageModel
             int para1=12;
 
             para1=para1/0;
+            
             Height=(int)Height / 100; // Convert height from cm to m
             BMI = Weight / (Height * Height);
             //todo: 尚未完成
